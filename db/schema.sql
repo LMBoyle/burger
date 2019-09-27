33a721da62a865ad7ce945db8c870398957b991f
Luke Boyle <boylelukem@gmail.com>
2019-09-27 11:36:55 -0400

    added to readme

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burgerName varchar(255) NOT NULL,
-  devoured BOOLEAN NOT NULL,
+  devoured BOOLEAN NOT NULL default false,
   PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 -- Insert a set of records --
 INSERT INTO burgers (burgerName) VALUES 
-  (''),
+  ('Olive And Let Die'),
+  ('Hit Me With Your Best Shallot'),
+  ('Beet-er Late Than Never');
