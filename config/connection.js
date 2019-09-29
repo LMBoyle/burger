8983453b6fc87a48c0a260dc22dfff66ee1d9657
Luke Boyle <boylelukem@gmail.com>
2019-09-29 12:13:11 -0400

    initial files connection.js and sql files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Dependencies =============================================================
+
+var mysql = require("mysql");
+
+// Connection ===============================================================
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export ===================================================================
+
+module.exports = connection;
