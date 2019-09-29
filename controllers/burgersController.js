4f6b162f354f17e271352723a8e244aeb9f9a480
Luke Boyle <boylelukem@gmail.com>
2019-09-29 12:43:07 -0400

    initial code in controller.js

---
+++ controllers/burgersController.js
@@ -0,0 +1,12 @@
+// Dependencies =============================================================
+
+var express = require("express");
+var router = express.Router();
+var connection = require("../models/burger.js");
+
+// Routes ===================================================================
+
+
+// Export ===================================================================
+
+module.exports = router;
