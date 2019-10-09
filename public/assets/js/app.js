f87e57b02d6ca17eb6c4284a8c2f3ec36393154c
Luke Boyle <boylelukem@gmail.com>
2019-10-09 17:38:34 -0400

    added function to prevent too many burgers on menu, plate or stomach

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -42,105 +42,120 @@ $(function () {
   // On submit, add a new burger
   $(".newBurger").on("submit", function(event) {
     event.preventDefault();
-
-    var newBurger = {
-      burgerName: $("#bur").val().trim(),
+    // Prevent too many burgers on menu
+    if ($("#menuList li").length > 26) {
+      alert("Too many items")
     }
-    console.log(newBurger)
-
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("New burger added");
-        location.reload();
+    else {
+      var newBurger = {
+        burgerName: $("#bur").val().trim(),
       }
-    )
+      console.log(newBurger)
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("New burger added");
+          location.reload();
+        }
+      )
+    }
   });
-
-  // TODO prevent too many burgers on plate or stomach
 
   // On menu click
   $(".burgerText").on("click", function(event) {
-    var id = $(this).data("id");
-    var numServed = $(this).data("served");
-    var numDevoured = $(this).data("devoured");
-
-    console.log("Num served: ", numServed)
-
-    var menuGifs = [
-      "https://media.giphy.com/media/l2SpNLZQSlENDKAkU/giphy.gif",
-      "https://media.giphy.com/media/3o7TKUGccxlaQqdgRO/giphy.gif",
-      "https://media.giphy.com/media/xTiTnuxMQhfXjhXQ4w/giphy.gif",
-      "https://cdn.dribbble.com/users/542042/screenshots/6010840/dribble-2.gif",
-      "https://media1.tenor.com/images/f8ebf501efc8a7a33e1cec808cd38e5f/tenor.gif?itemid=5333952",
-      "https://media1.tenor.com/images/8b6817632c4bb794a8f67c0870307b40/tenor.gif?itemid=4470280"
-    ]
-
-    var gifDiv = $(".menuGif")
-    var gifImg = $("<img>")
-
-    var newState = {
-      numServed: numServed + 1,
-      numDevoured : numDevoured
-    };
-
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newState
-    }).then(function() {
-      console.log("changed state: ", newState);
-      // Show gif 
-      gifDiv.empty();
-      gifImg.attr("src", menuGifs[Math.floor(Math.random() * menuGifs.length)])
-      gifDiv.append(gifImg)
-      $(".menuGif").css("display", "block");
-
-      // Wait 5 seconds then reload the page
-      setTimeout(reload, 5000)
-    })
+    // Prevent too many burgers on plate
+    if ($("#plateList li").length > 40) {
+      alert("Plates Full!")
+    }
+    else {
+      var id = $(this).data("id");
+      var numServed = $(this).data("served");
+      var numDevoured = $(this).data("devoured");
+  
+      console.log("Menu List Count ", )
+  
+      var menuGifs = [
+        "https://media.giphy.com/media/l2SpNLZQSlENDKAkU/giphy.gif",
+        "https://media.giphy.com/media/3o7TKUGccxlaQqdgRO/giphy.gif",
+        "https://media.giphy.com/media/xTiTnuxMQhfXjhXQ4w/giphy.gif",
+        "https://cdn.dribbble.com/users/542042/screenshots/6010840/dribble-2.gif",
+        "https://media1.tenor.com/images/f8ebf501efc8a7a33e1cec808cd38e5f/tenor.gif?itemid=5333952",
+        "https://media1.tenor.com/images/8b6817632c4bb794a8f67c0870307b40/tenor.gif?itemid=4470280"
+      ]
+  
+      var gifDiv = $(".menuGif")
+      var gifImg = $("<img>")
+  
+      var newState = {
+        numServed: numServed + 1,
+        numDevoured : numDevoured
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newState
+      }).then(function() {
+        console.log("changed state: ", newState);
+        // Show gif 
+        gifDiv.empty();
+        gifImg.attr("src", menuGifs[Math.floor(Math.random() * menuGifs.length)])
+        gifDiv.append(gifImg)
+        $(".menuGif").css("display", "block");
+  
+        // Wait 5 seconds then reload the page
+        setTimeout(reload, 5000)
+      })
+    }
   })
 
   // On plate click
   $(".burgerPlate").on("click", function(event) {
-    var id = $(this).data("id");
-    var numServed = $(this).data("served");
-    var numDevoured = $(this).data("devoured");
-
-    var plateGifs = [
-      "http://giphygifs.s3.amazonaws.com/media/AAfpHO80onDYk/giphy.gif",
-      "https://media.giphy.com/media/mEDJ5JPioxctLKo49q/giphy.gif",
-      "https://media.giphy.com/media/5YpIssZR9aNPFCKScs/giphy.gif",
-      "https://media.giphy.com/media/MmOAKMzYg4EDqadUCH/giphy.gif",
-      "https://media.giphy.com/media/39CaVn6lpm2F2AY9m6/giphy.gif",
-      "https://media1.tenor.com/images/f2e1ffd4ebb482d04ebb1d8098de9d64/tenor.gif?itemid=14603317"
-    ]
-
-    var gifDiv = $(".plateGif")
-    var gifImg = $("<img>")
-
-    var newState = {
-      numServed: numServed - 1,
-      numDevoured : numDevoured + 1
-    };
-
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newState
-    }).then(function() {
-      console.log("changed state: ", newState);
-      // Show gif 
-      gifDiv.empty();
-      gifImg.attr("src", plateGifs[Math.floor(Math.random() * plateGifs.length)])
-      gifDiv.append(gifImg)
-      $(".plateGif").css("display", "block");
-
-      // Wait 5 seconds then reload the page
-      setTimeout(reload, 5000)
-    })
-  })
-
+    // Prevent too many burgers in stomach
+    if ($("#stomachList li").length > 24) {
+      alert("Full!")
+    }
+    else {
+      var id = $(this).data("id");
+      var numServed = $(this).data("served");
+      var numDevoured = $(this).data("devoured");
+  
+      var plateGifs = [
+        "http://giphygifs.s3.amazonaws.com/media/AAfpHO80onDYk/giphy.gif",
+        "https://media.giphy.com/media/mEDJ5JPioxctLKo49q/giphy.gif",
+        "https://media.giphy.com/media/5YpIssZR9aNPFCKScs/giphy.gif",
+        "https://media.giphy.com/media/MmOAKMzYg4EDqadUCH/giphy.gif",
+        "https://media.giphy.com/media/39CaVn6lpm2F2AY9m6/giphy.gif",
+        "https://media1.tenor.com/images/f2e1ffd4ebb482d04ebb1d8098de9d64/tenor.gif?itemid=14603317"
+      ]
+  
+      var gifDiv = $(".plateGif")
+      var gifImg = $("<img>")
+  
+      var newState = {
+        numServed: numServed - 1,
+        numDevoured : numDevoured + 1
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newState
+      }).then(function() {
+        console.log("changed state: ", newState);
+        // Show gif 
+        gifDiv.empty();
+        gifImg.attr("src", plateGifs[Math.floor(Math.random() * plateGifs.length)])
+        gifDiv.append(gifImg)
+        $(".plateGif").css("display", "block");
+  
+        // Wait 5 seconds then reload the page
+        setTimeout(reload, 5000)
+      })
+    }
+  });
+  
   // Reload page
   function reload() {
     location.reload();
